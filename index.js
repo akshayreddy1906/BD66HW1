54015e3b6853b064cabcc0882781b61941883fa1
akshay <156815758+akshayreddy1906@users.noreply.github.com>
2025-01-01 19:46:36 +0000

    added packages

--- index.js
+++ index.js
@@ -1,92 +1,16 @@
-const request = require("supertest");
-const http = require("http");
-const { getAllEmployees } = require("../controllers"); 
-const { app } = require("../index");
-jest.mock("../controllers", () => ({
-...jest.requireActual("../controllers"), 
- getAllEmployees: jest. fn(),
-}));
-let server;
-beforeAll(async () => {
-    server = http.createServer(app); 
-    server.listen(3001);
+const cors = require("cors");
+const express = require( "express");
+const { getAllEmployees, getEmployeesById } = require("./controllers");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/employees", async (req, res) => { 
+  const employees = getAllEmployees(); 
+  res.json({ employees });
 });
-afterAll(async () => {
-    server.close();
+app.get("/employees/details/:id", async (req, res) => { 
+  let employee = getEmployeesById(parseInt(req.params.id));
+  res.json({employee});
 });
-describe("Controller Function tests", () => {
-    beforeEach(() => {
-    jest.clearAllMocks();
-});
- it("should return all employees", () => { 
- let mockedeEmployees = [
-     {
-         employeeId: 1,
-         name: 'Rahul Sharma',
-         email: 'rahul.sharma@example.com',
-         departmentId: 1,
-         roleId: 1,
-     },
-     {
-         employeeId: 2,
-         name: 'Priya Singh',
-         email: 'priya.singh@example.com',
-         departmentId: 2,
-         roleId: 2,
-     },
-     {
-         employeeId: 3,
-         name: 'Ankit Verma',
-         email: 'ankit.verma@example.com',
-         departmentId: 1,
-         roleId: 3,
-     },
-   ];
-     getAllEmployees.mockReturnValue (mockedeEmployees); 
-     let result = getAllEmployees();
-     expect(result).toEqual(mockedeEmployees);
-     expect(result.length).toBe(3);
-    });
-});
-describe("API Endpoint tests", () => {    
-    it("GET /employees should get all employees", async () => { 
-     const res = await request(server).get("/employees");
-     expect(res.status).toBe(200);
-     expect(res.body).toEqual({ employees: [
-           {
-               employeeId: 1,
-               name: 'Rahul Sharma',
-               email: 'rahul.sharma@example.com',
-               departmentId: 1,
-               roleId: 1,
-           },
-           {
-               employeeId: 2,
-               name: 'Priya Singh',
-               email: 'priya.singh@example.com',
-               departmentId: 2,
-               roleId: 2,
-           },
-           {   employeeId: 3,
-               name: 'Ankit Verma',
-               email: 'ankit.verma@example.com',
-               departmentId: 1,
-               roleId: 3,
-           },
-         ] 
-    });
-    });
-        expect(res.body.employees.length).toBe(3);
-    it("GET /employees/details/:id should get an employee by ID", async () => {
-        const res = await request(server).get("/employees/details/1");
-        expect(res.status).toBe(200);
-        expect(res.body).toEqual({ employee: {
-        employeeId: 1,
-        name: "Rahul Sharma",
-        email: "rahul.sharma@example.com",
-        departmentId: 1,
-        roleId: 1,
-        },
-        });
-    });  
-});
+module.exports = { app };
+I
